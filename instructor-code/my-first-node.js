f6dfca1b0dcd2f21f2a17265bba848798f826be5
Josiah Maina <josiah5115@gmail.com>
2018-07-04 11:03:14 -0600

    api end point set-up

--- instructor-code/my-first-node.js
+++ instructor-code/my-first-node.js
@@ -4,6 +4,14 @@ const port = 3005;
 
 app.use(express.static(__dirname + '/build'))
 
+function addStuff (x, y){
+  return x + y;
+}
+
+app.get('/api/calculate/add', (req, res) => {
+  res.send(42);
+})
+
 app.listen(port, ()=>{
   console.log('Server running on port: ' + port);
 })
