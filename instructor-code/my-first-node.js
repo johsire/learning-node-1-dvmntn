8bb058131cd87b62097e0bf50efead2b410932af
Josiah Maina <josiah5115@gmail.com>
2018-07-04 11:29:19 -0600

    modified files

--- instructor-code/my-first-node.js
+++ instructor-code/my-first-node.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = 3005;
 
+const app = express();
+
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/build'))
 
 function addStuff (x, y){
   return x + y;
 }
 
-app.get('/api/calculate/add', (req, res) => {
-  res.send(42);
+// Params
+app.get('/api/calculate/add/:x/:y', (req, res) => {
+  let total = req.params.x * 1 + req.params.y * 1
+
+  res.send({total:total});
+})
+
+// Body
+app.post('/api/calculate/subtract', (req, res) => {
+  console.log(req.body);
+  res.send('Request received');
 })
 
 app.listen(port, ()=>{
